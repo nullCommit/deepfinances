e10ac2980dbb559138002d3d626875d8b92712ab
Allan <allangeek@gmail.com>
2021-12-28 12:38:16 -0300

    Create global color theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Dashboard } from './src/screens/Dashboard/';
 
+import theme from './src/global/styles/theme';
+import { ThemeProvider } from 'styled-components';
+
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
