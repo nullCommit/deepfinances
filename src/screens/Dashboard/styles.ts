ca6d2f081642ac267625ef42e4a2556eb8e63d6d
Allan <allangeek@gmail.com>
2021-12-29 13:38:09 -0300

    Create HighlightCard and HighlightCards scrollview

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -16,6 +17,7 @@ export const UserWrapper = styled.View`
   align-items: center;
 
   padding: 0 24px;
+  padding-top: ${getStatusBarHeight() + RFValue(28)}px;
 `;
 
 export const Header = styled.View`
@@ -23,8 +25,6 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
 
   background-color: ${({ theme }) => theme.colors.primary};
-
-  justify-content: center;
 `;
 
 export const UserInfo = styled.View`
@@ -61,4 +61,15 @@ export const Icon = styled(Feather)`
   font-size: ${RFValue(24)}px;
 
   color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
