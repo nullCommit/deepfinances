29a93a280f889531597223f4952622155ebd0e68
Allan <allangeek@gmail.com>
2021-12-31 21:16:08 -0300

    Implemented navigation and customized tab menu

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-
-import { Register } from './src/screens/Register/';
-
 import {
   useFonts,
   Poppins_400Regular,
@@ -12,6 +9,8 @@ import {
 
 import theme from './src/global/styles/theme';
 import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +25,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
