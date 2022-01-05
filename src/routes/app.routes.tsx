955fa40f9e45ef32fc2afb0f0445fbbe0e0df6a4
Allan <allangeek@gmail.com>
2022-01-05 16:12:30 -0300

    Create Resume screen and history cards component

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,6 +6,7 @@ import { useTheme } from 'styled-components';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -49,7 +50,7 @@ export function AppRoutes() {
       />
       <Screen
         name='Resumo'
-        component={Dashboard}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name='pie-chart' size={size} color={color} />
