14a2dbec22abb0b8ed82850db03c4e84a5f64eb0
Allan <allangeek@gmail.com>
2022-01-06 16:07:54 -0300

    Add SignIn screen

--- App.tsx
+++ App.tsx
@@ -15,6 +15,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { StatusBar } from 'react-native';
 
+import { SignIn } from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -30,7 +32,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle='light-content' />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
