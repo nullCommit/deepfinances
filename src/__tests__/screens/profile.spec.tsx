57966bdef83c21104f2e24a79d53c070709ab2a2
Allan <allangeek@gmail.com>
2022-03-16 14:18:18 -0300

    Usa describe para montar suites de test

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,28 +3,30 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
-test('check whether or not the correctly user input placeholder is showing', () => {
-  const { getByPlaceholderText } = render(<Profile />);
+describe('Profile screen', () => {
+  it('should have the correctly user input placeholder name', () => {
+    const { getByPlaceholderText } = render(<Profile />);
 
-  const inputName = getByPlaceholderText('Nome');
+    const inputName = getByPlaceholderText('Nome');
 
-  expect(inputName).toBeTruthy();
-});
+    expect(inputName).toBeTruthy();
+  });
 
-test('check if user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />);
+  it('should have user data loaded', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const inputName = getByTestId('input-name');
-  const inputSurname = getByTestId('input-surname');
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
 
-  expect(inputName.props.value).toEqual('Allan');
-  expect(inputSurname.props.value).toEqual('Duarte');
-});
+    expect(inputName.props.value).toEqual('Allan');
+    expect(inputSurname.props.value).toEqual('Duarte');
+  });
 
-test('check if title render correctly', () => {
-  const { getByTestId } = render(<Profile />);
+  it('should have a correctly title', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const textTitle = getByTestId('text-title');
+    const textTitle = getByTestId('text-title');
 
-  expect(textTitle.props.children).toContain('Perfil');
+    expect(textTitle.props.children).toContain('Perfil');
+  });
 });
